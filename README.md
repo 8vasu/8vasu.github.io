a060e95c01eb54daa2520d743c7bfd40c477ff09
Soumendra Ganguly <soumendraganguly@gmail.com>
2023-06-30 14:56:51 -0500

    Fresh
    Signed-off-by: Soumendra Ganguly <soumendraganguly@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+Source code for my professional website (soumendraganguly.com). It is generated using Hugo, GNU Emacs Org-mode, and a custom theme.
---
+++ js/main.js
@@ -0,0 +1,6 @@
+if (document.body.scrollHeight > 2*(window.innerHeight)) {
+    document.body.style.backgroundImage = "repeating-radial-gradient(ellipse at center, transparent, transparent 1px, rgba(0, 0, 0, 0.6) 3px, transparent 5px),\
+	repeating-radial-gradient(circle at center, transparent, transparent 2px, rgba(0, 0, 0, 0.4) 4px, transparent 6px),\
+	radial-gradient(ellipse at center, black 2%, #05090f 3%, #05090f 5%, #2b4e77 7%, #2b4e77 45%, #05090f 92%, black 100%)";
+
+}
